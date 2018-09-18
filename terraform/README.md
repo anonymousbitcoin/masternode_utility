b736587cdb6296a60cc5ae9d294b9f551b0cfd6c
Brian de la Motte <bdelamotte2.0@gmail.com>
2018-09-17 21:43:33 -0400

    Initial commit

---
+++ terraform/README.md
@@ -0,0 +1,10 @@
+==Mac==
+* Clone this repo and cd into project
+* `brew install terraform`
+* cd terraform
+
+Automatically download and install any Provider binary for the providers in use within the configuration
+* `terraform init`
+* `terraform apply`
+* Type yes
+
---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  # TODO - Make region variable / input. MN's should be worldwide! Let user choose it.
+  region = "us-east-1"
+}
+
+# Generate a random public-private RSA key pair for EC2 instance
+resource "tls_private_key" "anon_masternode_key" {
+  algorithm = "RSA"
+}
+
+# Create AWS key pair
+resource "aws_key_pair" "anon_masternode_key_pair" {
+  key_name = "anon_masternode_key"
+  public_key = "${tls_private_key.anon_masternode_key.public_key_openssh}"
+}
+
+# Create EC2 instance with generated key pair
+resource "aws_instance" "masternode" {
+  ami = "ami-04681a1dbd79675a5"
+  # TODO - Make instance_type a variable / input.
+  instance_type = "t2.micro"
+  key_name = "anon_masternode_key"
+
+  # FIXME - Write out private key to a file so user can SSH into node for troubleshooting or fine-tuning
+  # Key name should go into .gitignore
+  provisioner "local-exec" {
+    command = "mkdir -p assets && echo '${tls_private_key.anon_masternode_key.private_key_pem}' > assets/anon_masternode.pem"
+  }
+
+  # Simple bootstrap of Anon MN
+  provisioner "remote-exec" {
+    
+    connection = {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${tls_private_key.anon_masternode_key.private_key_pem}"
+    }
+
+    inline = [
+      "sudo yum install -y git gcc",
+      "git clone https://github.com/anonymousbitcoin/anon/",
+      "cd ~/anon/anonutil && ./build.sh"
+      
+    ]
+  }
+}
+
